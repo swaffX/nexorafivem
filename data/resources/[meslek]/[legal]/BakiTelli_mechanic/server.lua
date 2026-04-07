7c7a1510122f492e7465293b2638156772c314a5
swaffX <swaffx@github.com>
2026-04-08 02:34:04 +0300

    mekanik

--- data/resources/[meslek]/[legal]/BakiTelli_mechanic/server.lua
+++ data/resources/[meslek]/[legal]/BakiTelli_mechanic/server.lua
@@ -1,9 +1,21 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
 AddEventHandler("BakiTelli_mechanic:buyitem")
 RegisterNetEvent("BakiTelli_mechanic:buyitem", function (price)
-    if getMoney(source) >= price then 
-        removeMoney(source, price)
-        TriggerClientEvent("BakiTelli_mechanic:cl:buyitem", source)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    
+    -- Mekanik görevine sahip oyuncular için fiyat kontrolü
+    if Player.PlayerData.job.name == "mechanic" then
+        price = 0 -- Mekanikler için ücretsiz
+    end
+    
+    if getMoney(src) >= price then 
+        if price > 0 then
+            removeMoney(src, price)
+        end
+        TriggerClientEvent("BakiTelli_mechanic:cl:buyitem", src)
     else 
-        nofity(source, Config.Langs["NoPrice"])
+        nofity(src, Config.Langs["NoPrice"])
     end
 end)
