08aaea0ffba9594ee7d51f093221577a9e59b41c
oguz <oguz@example.com>
2026-04-10 09:42:40 +0300

    swx_admin

---
+++ data/resources/[admin]/swx_admin/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'swaffX'
+description 'SWX Admin Panel - Cyberpunk Style'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
