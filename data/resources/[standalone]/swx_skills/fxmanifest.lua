9c2140fa86505dc54d0a904de3587627f2c2020e
swaffX <swaffx@github.com>
2026-04-11 00:35:16 +0300

    fafafaff

--- data/resources/[standalone]/swx_skills/fxmanifest.lua
+++ data/resources/[standalone]/swx_skills/fxmanifest.lua
@@ -7,7 +7,11 @@ version '1.0.0'
 
 shared_script 'config.lua'
 client_script 'client.lua'
-server_script 'server.lua'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
 
 ui_page 'html/index.html'
 
