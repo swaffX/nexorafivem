74e1e0adf5bf226eda966e271c59f02a4826fed8
swaffX <swaffx@github.com>
2026-04-12 13:32:00 +0300

    xdxdxdxd

--- data/resources/[exe-eklenti]/blips_creator/server/database.lua
+++ data/resources/[exe-eklenti]/blips_creator/server/database.lua
@@ -4,23 +4,34 @@ function setupDatabase(cb)
     if(resName ~= "blips_creator") then
         print("It would be appreciated using ^blips_creator^7 as name of the resource")
     end
-    
-    local scriptVersion = GetResourceMetadata(resName, 'version', 0)
 
-    local sqlPath = "sql/%s.sql"
+    -- Execute SQL directly
+    local sql = [[
+        CREATE TABLE IF NOT EXISTS `global_blips` (
+            `id` INT(11) NOT NULL AUTO_INCREMENT,
+            `name` VARCHAR(50) NOT NULL DEFAULT 'blip' COLLATE 'utf8mb4_general_ci',
+            `x` FLOAT(12) NOT NULL DEFAULT '0',
+            `y` FLOAT(12) NOT NULL DEFAULT '0',
+            `z` FLOAT(12) NOT NULL DEFAULT '0',
+            `streetName` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+            `sprite` SMALLINT(6) NOT NULL DEFAULT '0',
+            `scale` FLOAT(12) NOT NULL DEFAULT '0',
+            `alpha` TINYINT(4) UNSIGNED NOT NULL DEFAULT '0',
+            `color` SMALLINT(6) NOT NULL DEFAULT '0',
+            `ticked` BIT(1) NOT NULL DEFAULT b'0',
+            `outline` BIT(1) NOT NULL DEFAULT b'0',
+            `display` TINYINT(3) NOT NULL DEFAULT '0',
+            `identifier` VARCHAR(80) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+            PRIMARY KEY (`id`) USING BTREE
+        );
+        ALTER TABLE global_blips ADD COLUMN IF NOT EXISTS identifier VARCHAR(80) DEFAULT NULL;
+    ]]
 
-    local sqlContent = LoadResourceFile(resName, string.format(sqlPath, scriptVersion))
-
-    if(sqlContent) then
-        MySQL.Async.execute(sqlContent, {}, function(affectedRows)
-            print("[" .. GetCurrentResourceName() .. "] Database ready (SQL executed)")
-            cb()
-        end, function(err, query, params)
-            print("[" .. GetCurrentResourceName() .. "] Database setup failed: " .. tostring(err))
-            cb()
-        end)
-    else
-        print("[" .. GetCurrentResourceName() .. "] SQL file not found, skipping database setup")
+    MySQL.Async.execute(sql, {}, function(affectedRows)
+        print("[" .. GetCurrentResourceName() .. "] Database ready (SQL executed)")
         cb()
-    end
+    end, function(err, query, params)
+        print("[" .. GetCurrentResourceName() .. "] Database setup failed: " .. tostring(err))
+        cb()
+    end)
 end
