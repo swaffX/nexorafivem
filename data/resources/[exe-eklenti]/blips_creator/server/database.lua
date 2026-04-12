a622e4fce43023c836c423e7c48e47aff3b244db
swaffX <swaffx@github.com>
2026-04-12 13:32:53 +0300

    xdxdxd

--- data/resources/[exe-eklenti]/blips_creator/server/database.lua
+++ data/resources/[exe-eklenti]/blips_creator/server/database.lua
@@ -5,8 +5,8 @@ function setupDatabase(cb)
         print("It would be appreciated using ^blips_creator^7 as name of the resource")
     end
 
-    -- Execute SQL directly
-    local sql = [[
+    -- Execute SQL directly - split into two queries
+    local createTableSql = [[
         CREATE TABLE IF NOT EXISTS `global_blips` (
             `id` INT(11) NOT NULL AUTO_INCREMENT,
             `name` VARCHAR(50) NOT NULL DEFAULT 'blip' COLLATE 'utf8mb4_general_ci',
@@ -23,13 +23,21 @@ function setupDatabase(cb)
             `display` TINYINT(3) NOT NULL DEFAULT '0',
             `identifier` VARCHAR(80) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
             PRIMARY KEY (`id`) USING BTREE
-        );
-        ALTER TABLE global_blips ADD COLUMN IF NOT EXISTS identifier VARCHAR(80) DEFAULT NULL;
+        )
     ]]
 
-    MySQL.Async.execute(sql, {}, function(affectedRows)
-        print("[" .. GetCurrentResourceName() .. "] Database ready (SQL executed)")
-        cb()
+    local alterTableSql = [[
+        ALTER TABLE global_blips ADD COLUMN IF NOT EXISTS identifier VARCHAR(80) DEFAULT NULL
+    ]]
+
+    MySQL.Async.execute(createTableSql, {}, function(affectedRows)
+        MySQL.Async.execute(alterTableSql, {}, function(affectedRows)
+            print("[" .. GetCurrentResourceName() .. "] Database ready (SQL executed)")
+            cb()
+        end, function(err, query, params)
+            print("[" .. GetCurrentResourceName() .. "] Database setup failed: " .. tostring(err))
+            cb()
+        end)
     end, function(err, query, params)
         print("[" .. GetCurrentResourceName() .. "] Database setup failed: " .. tostring(err))
         cb()
